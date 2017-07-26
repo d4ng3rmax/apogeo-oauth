f3623a415adcd66cd16e85966f3ceab1d12ee07f
gsilveiratv <gabriel.silveira@tevec.com.br>
2017-07-26 00:42:26 -0300

    datatable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgxPaginationModule } from 'ngx-pagination';
-import { Ng2TableModule } from 'ng2-table/ng2-table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question/question.component';
 import { QuestionListComponent } from './question-list/question-list.component';
 import { HeaderComponent } from './header/header.component';
+import { DataTableDirective } from './shared/directives/data-table.directive';
 
 
 @NgModule({
@@ -17,14 +17,14 @@ import { HeaderComponent } from './header/header.component';
         AppComponent,
         QuestionComponent,
         QuestionListComponent,
-        HeaderComponent
+        HeaderComponent,
+        DataTableDirective
     ],
     imports: [
         BrowserModule,
         FormsModule,
         HttpModule,
         NgxPaginationModule,
-        Ng2TableModule,
         AppRoutingModule
     ],
     providers: [],
