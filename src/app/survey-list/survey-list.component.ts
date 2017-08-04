cef0f6001d48d3ee630e0219a1dc36523f367f21
gsilveiratv <gabriel.silveira@tevec.com.br>
2017-08-03 23:25:31 -0300

    finish survey adjusts

--- src/app/survey-list/survey-list.component.ts
+++ src/app/survey-list/survey-list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, Input, OnInit } from '@angular/core';
+import { DataGridSurveyComponent } from './../shared/data-grid-survey/data-grid.component';
+import { Subject } from 'rxjs/Rx';
 
 @Component({
-  selector: 'app-survey-list',
-  templateUrl: './survey-list.component.html',
-  styleUrls: ['./survey-list.component.scss']
+    selector: 'app-survey-list',
+    templateUrl: './survey-list.component.html',
+    styleUrls: ['./../shared/data-grid/data-grid.component.scss']
 })
 export class SurveyListComponent implements OnInit {
 
-  constructor() { }
+    @ViewChild( DataGridSurveyComponent ) dataGrid : DataGridSurveyComponent;
 
-  ngOnInit() {
-  }
+    active : boolean;
+
+    constructor() {
+        this.active = true;
+    }
+    ngOnInit() {}
+
+    public onSearch = ( value ) : void => {
+        this.dataGrid.onSearch( value, this.active );
+    }
+
+    public clearFilter = ( search ) : void => {
+        this.dataGrid.clearFilter();
+        search.reset();
+    }
 
 }
